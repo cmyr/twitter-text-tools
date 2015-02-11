bd09aed07c9f07ef31b1299681ec8fe571dce623
cmyr <colin@cmyr.net>
2015-02-11 17:04:15 -0500

    added prune_dict function

--- twittertools/utils.py
+++ twittertools/utils.py
@@ -60,8 +60,26 @@ def dict_from_dbm(dbm_tweet):
     except ValueError:
         return None
 
+
+def prune_dict(indict, dict_template):
+    """
+    given two dictionaries, returns a new dictionary with those key/value
+    pairs from the first dictionary that are present in the second. If value
+    is a dictionary, performs recursively.
+    """
+
+    outdict = dict()
+    for key, value in dict_template.items():
+        keep = indict.get(key)
+        if isinstance(keep, dict) and isinstance(value, dict):
+            outdict[key] = prune_dict(keep, value)
+        else:
+            outdict[key] = keep
+    return outdict
+
+
 def main():
-	pass
+    pass
 
 
 if __name__ == "__main__":
