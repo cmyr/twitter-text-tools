573c1a3109c2576918ff4f2e1b9d03f6d77bfc00
cmyr <colin@cmyr.net>
2016-03-09 18:52:56 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='twittertools',
+      version='0.1',
+      description='python twitter text tools',
+      author='Colin Rofls',
+      author_email='colin@cmyr.net',
+      url='https://github.com/cmyr/twitter-text-tools',
+      packages=['twittertools']
+      )
